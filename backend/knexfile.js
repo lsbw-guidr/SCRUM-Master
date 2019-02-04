164949f3812e39cee59242251da2bf00184a6352
iAmAdamReid <adam@adamreid.me>
2019-02-04 10:06:04 -0800

    Add knex and sqlite3 dependencies

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -5,10 +5,30 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './data/guidr.sqlite3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
     }
   },
-
+  testing: {
+		client: 'sqlite3',
+		connection: {
+			filename: './data/test-guidr.sqlite3'
+		},
+		useNullAsDefault: true,
+		migrations: {
+			directory: './data/migrations'
+		},
+		seeds: {
+			directory: './data/seeds'
+		}
+  },
+  
   staging: {
     client: 'postgresql',
     connection: {
