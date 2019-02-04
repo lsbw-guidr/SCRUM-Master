b7a62668653ecabb0f55143099649546fd4609ec
iAmAdamReid <adam@adamreid.me>
2019-02-04 10:48:57 -0800

    Modify index.js for server testing

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,34 @@
-const express = require('express');
-const app = express();
-const port = 3333;
+// const express = require('express');
+// const app = express();
+// const port = 3333;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+// app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+const server = require('./api/server');
+
+server.get('/', (req, res) => res.send("Index directory for Guidr API."));
+
+// const { Client } = require('pg');
+
+// const client = new Client({
+// 	connectionString: process.env.DATABASE_URL,
+// 	ssl: true
+// });
+
+// client.connect();
+
+// client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+// 	if (err) throw err;
+// 	for (let row of res.rows) {
+// 		console.log(JSON.stringify(row));
+// 	}
+// 	client.end();
+// });
+
+// console.log(process.env.DB_ENV);
+
+const port = process.env.PORT || 8000;
+
+server.listen(port, () => console.log(`Server listening on ${port}`));
